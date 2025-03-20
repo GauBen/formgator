4fd597194b9625a9c4b7a41c63718ff059a36fa7
Gautier Ben Aim <benaim.gautier@gmail.com>
2025-03-20 20:35:51 +0100

    feat:  validator

---
+++ src/validators/multi.test.ts
@@ -0,0 +1,62 @@
+import { describe, it } from "node:test";
+import assert from "../assert.ts";
+import { failures, safeParse, succeed } from "../definitions.ts";
+import { multi } from "./multi.ts";
+
+describe("multi()", async () => {
+  it("should accept valid inputs", () => {
+    const data = new FormData();
+    data.append("input", "hello world!");
+    data.append("input", "  hello ");
+    data.append("input", "");
+
+    assert.deepEqualTyped(
+      multi()[safeParse](data, "input"),
+      succeed(["hello world!", "  hello ", ""]),
+    );
+
+    assert.deepEqualTyped(
+      multi({ min: 3, max: 3 })[safeParse](data, "input"),
+      succeed(["hello world!", "  hello ", ""]),
+    );
+
+    assert.deepEqualTyped(
+      multi()
+        .map((value) => value.trim())
+        [safeParse](data, "input"),
+      succeed(["hello world!", "hello", ""]),
+    );
+
+    assert.deepEqualTyped(
+      multi()
+        .map((value) => value.length)
+        [safeParse](data, "input"),
+      succeed([12, 8, 0]),
+    );
+
+    assert.deepEqualTyped(multi()[safeParse](data, "missing"), succeed([]));
+  });
+
+  it("should refuse invalid inputs", () => {
+    const data = new FormData();
+    data.append("input", "hello world!");
+    data.append("input", "123");
+    data.append("input", "");
+    data.append("file", new File(["hmm"], "file.txt"));
+
+    assert.deepEqualTyped(multi()[safeParse](data, "file"), failures.type());
+    assert.deepEqualTyped(multi({ min: 4 })[safeParse](data, "input"), failures.minlength(4));
+    assert.deepEqualTyped(multi({ max: 2 })[safeParse](data, "input"), failures.maxlength(2));
+    assert.deepEqualTyped(
+      multi()
+        .map(
+          () => {
+            throw new Error("Custom error");
+          },
+          (error) => (error as Error).message,
+        )
+        [safeParse](data, "input"),
+      failures.transform("Custom error"),
+    );
+  });
+});
